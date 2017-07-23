b20022f6905a766b149aee85cef94e9708c796c7
cw <953098507@qq.com>
2017-07-23 15:09:18 +0800

    增加大小与比例控制，处理事件冲突等

--- app/src/main/java/com/cw/slideswitch/view/SlideButton.java
+++ app/src/main/java/com/cw/slideswitch/view/SlideButton.java
@@ -5,7 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.media.ThumbnailUtils;
+import android.graphics.Matrix;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -23,16 +23,19 @@ import com.cw.slideswitch.R;
 public class SlideButton extends View {
 
     private Scroller mScroller;
-    private Bitmap mBackground;
-    private Bitmap mSlideButton;
     private boolean mIsOpen;
     private int mOpenBackground = R.drawable.widget_icon_slidebutton_yellow_bg;
     private int mCloseBackground = R.drawable.widget_icon_slidebutton_write_bg;
     private int mSlideImgId = R.drawable.widget_icon_slidebutton_write_slider;
-    private int mSildLeft;
+    private int mSlideLeft;
     private int mMaxLeft;
     private int mStartX;
     private int mMoveX;
+
+    private Bitmap mOpenBitmap;
+    private Bitmap mCloseBitmap;
+    private Bitmap mBackground;
+    private Bitmap mSlideBitmap;
 
 
     public SlideButton(Context context) {
@@ -70,47 +73,64 @@ public class SlideButton extends View {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(mBackground.getWidth(), mBackground.getHeight());
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+        //计算宽高比
+        float v = (float) mBackground.getWidth() / mBackground.getHeight();
+        if (widthSpecMode == MeasureSpec.EXACTLY
+                && heightSpecMode == MeasureSpec.EXACTLY) {
+            setMeasuredDimension(widthSpecSize, heightSpecSize);
+        } else if (widthSpecMode == MeasureSpec.EXACTLY) {
+            setMeasuredDimension(widthSpecSize, (int) (widthSpecSize / v));
+        } else if (heightSpecMode == MeasureSpec.EXACTLY) {
+            setMeasuredDimension((int) (widthSpecSize * v), heightSpecMode);
+        } else {
+            setMeasuredDimension(mBackground.getWidth(), mBackground.getHeight());
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        //以测量长度与原图长度相差较小的一边为基准缩放bitmap
+        if (Math.abs(w - mBackground.getWidth()) < Math.abs(h - mBackground.getHeight())) {
+            setBitMapSize((float) w / mBackground.getWidth());
+        } else {
+            setBitMapSize((float) h / mBackground.getHeight());
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(mBackground, 0, 0, null);
-        canvas.drawBitmap(mSlideButton, mSildLeft, 0, null);
+        canvas.drawBitmap(mSlideBitmap, mSlideLeft, 0, null);
     }
 
     @Override
     public void computeScroll() {
         if (mScroller.computeScrollOffset()) {
-            mSildLeft = mScroller.getCurrX();
+            mSlideLeft = mScroller.getCurrX();
             invalidate();
         }
     }
 
     private void init() {
+        mOpenBitmap = BitmapFactory.decodeResource(getResources(), mOpenBackground);
+        mCloseBitmap = BitmapFactory.decodeResource(getResources(), mCloseBackground);
+        mSlideBitmap = BitmapFactory.decodeResource(getResources(), mSlideImgId);
         setBackground(mIsOpen);
-        setSlideImgId(mSlideImgId);
-        //保证高度相同
-        mSlideButton = ThumbnailUtils.extractThumbnail(mSlideButton, mBackground.getHeight(), mBackground.getHeight());
-        mMaxLeft = mBackground.getWidth() - mSlideButton.getWidth();
-
-        if (this.mIsOpen) {
-            //如果是打开状态
-            mSildLeft = mMaxLeft;
-        } else {
-            //如果是关闭状态
-            mSildLeft = 0;
-        }
 
         //点击改变开关状态
         this.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mIsOpen) {
-                    mScroller.startScroll(mSildLeft, 0, -mSildLeft, 0, 500);
+                    mScroller.startScroll(mSlideLeft, 0, -mSlideLeft, 0, 500);
                     mIsOpen = false;
                 } else {
-                    mScroller.startScroll(mSildLeft, 0, mMaxLeft - mSildLeft, 0, 500);
+                    mScroller.startScroll(mSlideLeft, 0, mMaxLeft - mSlideLeft, 0, 500);
                     mIsOpen = true;
                 }
                 setBackground(mIsOpen);
@@ -136,12 +156,12 @@ public class SlideButton extends View {
                 //记录每次移动的距离
                 mMoveX += diffX;
                 //按钮左边距更具手指坐标改变而改变
-                mSildLeft += diffX;
+                mSlideLeft += diffX;
                 //不能超过最大值
-                if (mSildLeft > mMaxLeft) {
-                    mSildLeft = mMaxLeft;
-                } else if (mSildLeft < 0) {
-                    mSildLeft = 0;
+                if (mSlideLeft > mMaxLeft) {
+                    mSlideLeft = mMaxLeft;
+                } else if (mSlideLeft < 0) {
+                    mSlideLeft = 0;
                 }
                 invalidate();
                 mStartX = endX;
@@ -154,9 +174,9 @@ public class SlideButton extends View {
                         mIsOpen = !mIsOpen;
                     }
                     if (mIsOpen) {
-                        mScroller.startScroll(mSildLeft, 0, mMaxLeft - mSildLeft, 0, 500);
+                        mScroller.startScroll(mSlideLeft, 0, mMaxLeft - mSlideLeft, 0, 500);
                     } else {
-                        mScroller.startScroll(mSildLeft, 0, -mSildLeft, 0, 500);
+                        mScroller.startScroll(mSlideLeft, 0, -mSlideLeft, 0, 500);
                     }
                     setBackground(mIsOpen);
                     invalidate();
@@ -172,19 +192,43 @@ public class SlideButton extends View {
         return !isOnClick || super.onTouchEvent(event);
     }
 
-    /**
-     * 设置背景
-     */
-    private void setBackground(boolean isOpen) {
-        mBackground = BitmapFactory.decodeResource(getResources()
-                , isOpen ? mOpenBackground : mCloseBackground);
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        //处理事件冲突，﻿请求父容器不要拦截事件，然后直接交给onTouchEvent处理
+        getParent().requestDisallowInterceptTouchEvent(true);
+        return super.dispatchTouchEvent(event);
     }
 
     /**
-     * 设置滑块图片
+     * 设置背景
+     *
+     * @param isOpen isOpen
      */
-    private void setSlideImgId(int backgroundResours) {
-        mSlideButton = BitmapFactory.decodeResource(getResources(), backgroundResours);
+    private void setBackground(boolean isOpen) {
+        mBackground = isOpen ? mOpenBitmap : mCloseBitmap;
+    }
+
+    /**
+     * 设置Bitmap缩放比例
+     *
+     * @param scale scale
+     */
+    private void setBitMapSize(float scale) {
+        Matrix matrix = new Matrix();
+        matrix.postScale(scale, scale);//x y 对应宽高缩小的比例，比如matrix.postScale(0.99f, 0.99f)
+        mOpenBitmap = Bitmap.createBitmap(mOpenBitmap, 0, 0, mOpenBitmap.getWidth(), mOpenBitmap.getHeight(), matrix, false);
+        mCloseBitmap = Bitmap.createBitmap(mCloseBitmap, 0, 0, mCloseBitmap.getWidth(), mCloseBitmap.getHeight(), matrix, false);
+        mSlideBitmap = Bitmap.createBitmap(mSlideBitmap, 0, 0, mSlideBitmap.getWidth(), mSlideBitmap.getHeight(), matrix, false);
+        setBackground(mIsOpen);
+
+        mMaxLeft = mBackground.getWidth() - mSlideBitmap.getWidth();
+        if (this.mIsOpen) {
+            //如果是打开状态
+            mSlideLeft = mMaxLeft;
+        } else {
+            //如果是关闭状态
+            mSlideLeft = 0;
+        }
     }
 
     /**
